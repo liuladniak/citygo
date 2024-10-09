80184a6b9470491af235c419413a4e5843388160
Liu Ladniak <liuladniak@gmail.com>
2024-10-09 14:54:19 -0400

    added vite config

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// export default defineConfig({
+export default {
   plugins: [react()],
   build: {
     rollupOptions: {
       external: ["react-router-dom"],
     },
   },
-});
+};
