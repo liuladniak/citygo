e7b984e10702044ca3696c45000a707a6029bf69
Liu Ladniak <liuladniak@gmail.com>
2024-10-14 19:50:44 -0400

    inspecting bug

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -50,7 +50,7 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   const { user_id, tour_id, number_of_people, booking_date } = req.body;
-
+  console.log("Incoming booking data:", req.body);
   try {
     const [id] = await knex("bookings").insert({
       user_id,
