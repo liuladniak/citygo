bde625038d02c2938cce0f6690cb390a05a888bd
Liu Ladniak <liuladniak@gmail.com>
2024-11-01 11:25:07 -0400

    fixing tsconfig to work with both jsx and tsx

--- dashboard/src/pages/Tours/Tours.tsx
+++ dashboard/src/pages/Tours/Tours.tsx
@@ -1,4 +1,4 @@
-import Header from "../../components/Header/Header";
+import Header from "../../components/Header/Header.js";
 import "./Tours.scss";
 import ToursList from "../../components/ToursList/ToursList.jsx";
 
