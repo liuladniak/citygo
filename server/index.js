826bb8c72a81b01937aa4573a8577b4366d6c738
liuladniak <ladniakliudmyla@gmail.com>
2025-02-18 18:09:50 -0500

    resolving cors policy

--- server/index.js
+++ server/index.js
@@ -58,6 +58,13 @@ app.use(
 
 app.use(express.json());
 
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", allowedOrigins);
+//   res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/users/", userRoutes);
 
