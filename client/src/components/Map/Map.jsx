1e4a1034b764c628378d6132d157743823a9d860
liuladniak <ladniakliudmyla@gmail.com>
2025-02-20 22:38:05 -0500

    fixing map instance cleanup error

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -37,26 +37,23 @@ const Map = ({
   itinerary = [],
   landmarks = [],
 }) => {
+  // useEffect(() => {
+  //   return () => {
+  //     const mapContainer = document.getElementById("map");
+  //     if (mapContainer && mapContainer._leaflet_id) {
+  //       mapContainer._leaflet_id = null;
+  //     }
+  //   };
+  // }, []);
+
   useEffect(() => {
     return () => {
       const mapContainer = document.getElementById("map");
       if (mapContainer && mapContainer._leaflet_id) {
-        mapContainer._leaflet_id = null;
+        delete mapContainer._leaflet_id; // Ensures no reused ID issues
       }
     };
   }, []);
-
-  // useEffect(() => {
-  //   return () => {
-  //     const mapElement = document.getElementById("map");
-  //     if (mapElement && mapElement._leaflet_id) {
-  //       const mapInstance = mapElement._leaflet_id;
-  //       if (mapInstance && mapInstance.remove) {
-  //         mapInstance.remove(); // Properly destroy the Leaflet map
-  //       }
-  //     }
-  //   };
-  // }, []);
 
   const getMarkerIcon = (category) => {
     switch (category) {
@@ -79,93 +76,96 @@ const Map = ({
         return redIcon;
     }
   };
-  const mapKey = `${latitude}-${longitude}-${itinerary.length}`;
+  const mapKey = `${latitude}-${longitude}-${itinerary.length}-${tours.length}-${landmarks.length}`;
+
   return (
     <div className="map-wrp">
       <div className={`map ${className}`}>
-        <MapContainer
-          key={mapKey}
-          id="map"
-          center={[latitude || 41.0082, longitude || 28.9784]}
-          zoom={13}
-          style={{ height: "100%" }}
-          scrollWheelZoom={false}
-        >
-          <TileLayer
-            url="https://tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token=ZnLcneIbh8Ixa1isCwwnIY9DnyIm8bsqPzZmMHxfvuiZytP2vhlUbBIW67hhJsTx"
-            attribution='<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
+        {latitude && longitude && (
+          <MapContainer
+            key={mapKey}
+            id="map"
+            center={[latitude || 41.0082, longitude || 28.9784]}
+            zoom={13}
+            style={{ height: "100%" }}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              url="https://tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token=ZnLcneIbh8Ixa1isCwwnIY9DnyIm8bsqPzZmMHxfvuiZytP2vhlUbBIW67hhJsTx"
+              attribution='<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
 
-          {itinerary.length > 1 && <AdjustMapBounds itinerary={itinerary} />}
+            {itinerary.length > 1 && <AdjustMapBounds itinerary={itinerary} />}
 
-          {latitude && longitude && (
-            <Marker
-              position={[latitude, longitude]}
-              icon={getMarkerIcon(category)}
-            >
-              <Popup>{popupText}</Popup>
-            </Marker>
-          )}
-
-          {tours.length > 0 &&
-            tours.map((tour) => (
+            {latitude && longitude && (
               <Marker
-                key={tour.id}
-                position={[
-                  parseFloat(tour.latitude),
-                  parseFloat(tour.longitude),
-                ]}
-                icon={getMarkerIcon(tour.category)}
+                position={[latitude, longitude]}
+                icon={getMarkerIcon(category)}
               >
-                <Popup>{tour.tour_name}</Popup>
+                <Popup>{popupText}</Popup>
               </Marker>
-            ))}
+            )}
 
-          {itinerary.length > 1 && (
-            <>
-              <Polyline
-                positions={itinerary
-                  .slice(0, -1)
-                  .map((stop) => [
-                    parseFloat(stop.latitude),
-                    parseFloat(stop.longitude),
-                  ])}
-                pathOptions={{ color: "blue", weight: 4 }}
-              />
-
-              {itinerary.map((stop, index) => (
+            {tours.length > 0 &&
+              tours.map((tour) => (
                 <Marker
-                  key={stop.id}
-                  position={[stop.latitude, stop.longitude]}
-                  icon={redIcon}
+                  key={tour.id}
+                  position={[
+                    parseFloat(tour.latitude),
+                    parseFloat(tour.longitude),
+                  ]}
+                  icon={getMarkerIcon(tour.category)}
                 >
-                  <Popup>{stop.name}</Popup>
+                  <Popup>{tour.tour_name}</Popup>
                 </Marker>
               ))}
-            </>
-          )}
 
-          {landmarks.map((landmark) => (
-            <Marker
-              key={landmark.id}
-              position={[landmark.latitude, landmark.longitude]}
-              icon={getMarkerIcon(landmark.category)}
-              eventHandlers={{
-                mouseover: (e) => {
-                  e.target.openPopup();
-                },
-                mouseout: (e) => {
-                  e.target.closePopup();
-                },
-              }}
-            >
-              <Popup>
-                <strong>{landmark.name}</strong>
-                <p>{landmark.description}</p>
-              </Popup>
-            </Marker>
-          ))}
-        </MapContainer>
+            {itinerary.length > 1 && (
+              <>
+                <Polyline
+                  positions={itinerary
+                    .slice(0, -1)
+                    .map((stop) => [
+                      parseFloat(stop.latitude),
+                      parseFloat(stop.longitude),
+                    ])}
+                  pathOptions={{ color: "blue", weight: 4 }}
+                />
+
+                {itinerary.map((stop, index) => (
+                  <Marker
+                    key={stop.id}
+                    position={[stop.latitude, stop.longitude]}
+                    icon={redIcon}
+                  >
+                    <Popup>{stop.name}</Popup>
+                  </Marker>
+                ))}
+              </>
+            )}
+
+            {landmarks.map((landmark) => (
+              <Marker
+                key={landmark.id}
+                position={[landmark.latitude, landmark.longitude]}
+                icon={getMarkerIcon(landmark.category)}
+                eventHandlers={{
+                  mouseover: (e) => {
+                    e.target.openPopup();
+                  },
+                  mouseout: (e) => {
+                    e.target.closePopup();
+                  },
+                }}
+              >
+                <Popup>
+                  <strong>{landmark.name}</strong>
+                  <p>{landmark.description}</p>
+                </Popup>
+              </Marker>
+            ))}
+          </MapContainer>
+        )}
       </div>
     </div>
   );
