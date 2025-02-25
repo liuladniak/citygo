267ef1cedd23a0f6d38f9f9f8304f0c7cbaf2e8b
liuladniak <ladniakliudmyla@gmail.com>
2025-02-25 08:41:27 -0500

    changed payment method to creating payment intent

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,8 +1,11 @@
 import express from "express";
 import Stripe from "stripe";
 import dotenv from "dotenv";
-
-dotenv.config();
+import axios from "axios";
+import "dotenv/config";
+import initKnex from "knex";
+import knexConfig from "../knexfile.js";
+const knex = initKnex(knexConfig[process.env.NODE_ENV || "development"]);
 const router = express.Router();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const CLIENT_URLS = process.env.CLIENT_URLS.split(",");
@@ -12,64 +15,44 @@ const getClientUrl = (req) => {
   return CLIENT_URLS.includes(origin) ? origin : CLIENT_URLS[0];
 };
 
-router.post("/create-checkout-session", async (req, res) => {
-  console.log("Received request to create checkout session");
-  console.log(req.body);
-  const {
-    user_id,
-    tour_id,
-    time_slot_id,
-    booking_date,
-    adults,
-    children,
-    infants,
-    mainImage,
-    price,
-    title,
-  } = req.body.bookings[0];
-
-  const adultPrice = price * adults;
-  const childPrice = price * 0.5 * children;
-  const infantPrice = 0 * infants;
-
-  const totalPrice = adultPrice + childPrice + infantPrice;
-
-  console.log(
-    "Req body:",
-    user_id,
-    tour_id,
-    time_slot_id,
-    booking_date,
-    adults,
-    children,
-    infants,
-    mainImage,
-    price,
-    title
-  );
-
+router.post("/create-payment-intent", async (req, res) => {
   try {
-    const clientUrl = getClientUrl(req);
+    console.log("Received request to create payment intent", req.body);
 
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      line_items: [
-        {
-          price_data: {
-            currency: "usd",
-            product_data: {
-              name: title,
-              images: [`${clientUrl}/${mainImage}`],
-            },
-            unit_amount: totalPrice * 100,
-          },
-          quantity: 1,
-        },
-      ],
-      mode: "payment",
-      success_url: `${clientUrl}/success`,
-      cancel_url: `${clientUrl}/cancel`,
-      metadata: {
+    const { bookings, selectedCurrency } = req.body;
+    console.log("Bookings as req body on the server", bookings);
+    if (!bookings || bookings.length === 0) {
+      return res.status(400).json({ error: "No bookings provided." });
+    }
+
+    const exchangeRates = await axios
+      .get(`https://api.exchangerate-api.com/v4/latest/USD`)
+      .catch((err) => {
+        console.error("Error fetching exchange rates:", err);
+        return null;
+      });
+
+    if (!exchangeRates.data.rates[selectedCurrency]) {
+      return res.status(400).json({ error: "Unsupported currency" });
+    }
+
+    const conversionRate =
+      exchangeRates.data.rates[selectedCurrency.toUpperCase()] || 1;
+
+    const tourIds = bookings.map((booking) => booking.tour_id);
+
+    const tours = await knex("tours")
+      .select("id", "price", "featured")
+      .whereIn("id", tourIds);
+
+    if (tours.length === 0) {
+      return res.status(404).json({ error: "No valid tours found." });
+    }
+    let totalAmount = 0;
+    let metadata = {};
+
+    bookings.forEach((booking, index) => {
+      const {
         user_id,
         tour_id,
         time_slot_id,
@@ -77,27 +60,52 @@ router.post("/create-checkout-session", async (req, res) => {
         adults,
         children,
         infants,
-      },
+      } = booking;
+
+      const tour = tours.find((t) => t.id === tour_id);
+      if (!tour) {
+        return res.status(400).json({ error: `Tour ID ${tour_id} not found.` });
+      }
+
+      let { price, featured } = tour;
+
+      const adultPrice = price * adults;
+      const childPrice = price * 0.5 * children;
+      const infantPrice = 0 * infants;
+      let totalPrice = adultPrice + childPrice + infantPrice;
+
+      if (featured) {
+        totalPrice *= 0.9;
+      }
+
+      totalAmount += totalPrice;
+
+      metadata[`booking_${index + 1}_user_id`] = user_id;
+      metadata[`booking_${index + 1}_tour_id`] = tour_id;
+      metadata[`booking_${index + 1}_time_slot_id`] = time_slot_id;
+      metadata[`booking_${index + 1}_booking_date`] = booking_date;
+      metadata[`booking_${index + 1}_adults`] = adults;
+      metadata[`booking_${index + 1}_children`] = children;
+      metadata[`booking_${index + 1}_infants`] = infants;
+    });
+    console.log("Total Amount:", totalAmount);
+    console.log("Metadata intent:", metadata);
+    const totalAmountConverted = Math.round(totalAmount * conversionRate * 100);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: totalAmountConverted,
+      currency: selectedCurrency,
+      metadata,
     });
 
-    console.log(
-      "Price:",
-      price,
-      "Metadata",
-      user_id,
-      tour_id,
-      booking_date,
-      time_slot_id,
-      adults,
-      children,
-      infants,
-      "metadata req body",
-      req.body
-    );
-    res.json({ id: session.id });
+    res.json({ clientSecret: paymentIntent.client_secret });
   } catch (error) {
-    console.error("Stripe Checkout Error:", error);
-    res.status(500).json({ error: error.message });
+    console.error("Error creating payment intent:", error);
+    console.error(
+      "Error details:",
+      error.response ? error.response.data : error.message
+    );
+    res.status(500).json({ error: "Failed to create payment intent." });
   }
 });
 
--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -4,50 +4,76 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 import initKnex from "knex";
 import knexConfig from "../knexfile.js";
 const knex = initKnex(knexConfig[process.env.NODE_ENV || "development"]);
-
+import "dotenv/config";
 const router = express.Router();
-router.post("/", async (req, res) => {
-  console.log("Webhook hit");
-  const sig = req.headers["stripe-signature"];
-  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+router.post(
+  "/",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
+    console.log("Webhook hit");
+    const sig = req.headers["stripe-signature"];
+    const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
-  let event;
+    let event;
 
-  try {
-    event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
-    console.log("Stripe Event:", event.type);
-    if (event.type === "checkout.session.completed") {
-      const session = event.data.object;
+    try {
+      event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+      console.log("Stripe Event:", event.type);
+      if (event.type === "payment_intent.succeeded") {
+        const paymentIntent = event.data.object;
+        console.log("Event data object:", paymentIntent);
+        const metadata = paymentIntent.metadata;
 
-      const metadata = session.metadata;
+        console.log("Metadata received:", metadata);
 
-      console.log("Metadata received:", metadata);
+        if (!metadata || !metadata.booking_1_user_id) {
+          throw new Error("Metadata missing or invalid");
+        }
 
-      if (!metadata || !metadata.user_id) {
-        throw new Error("Metadata missing or invalid");
+        await addBookingsToDatabase(metadata);
       }
-
-      await addBookingToDatabase(metadata);
+      res.json({ received: true });
+    } catch (err) {
+      console.error("Webhook signature verification failed.", err.message);
+      return res.status(400).send(`Webhook Error: ${err.message}`);
     }
-    res.json({ received: true });
-  } catch (err) {
-    console.error("Webhook signature verification failed.", err.message);
-    return res.status(400).send(`Webhook Error: ${err.message}`);
   }
-});
+);
 
-async function addBookingToDatabase(metadata) {
-  await knex("bookings").insert({
-    user_id: parseInt(metadata.user_id),
-    tour_id: parseInt(metadata.tour_id),
-    time_slot_id: parseInt(metadata.time_slot_id),
-    booking_date: metadata.booking_date,
-    adults: parseInt(metadata.adults),
-    children: parseInt(metadata.children),
-    infants: parseInt(metadata.infants),
-    created_at: new Date(),
-    updated_at: new Date(),
-  });
+async function addBookingsToDatabase(metadata) {
+  console.log("Adding bookings to database:", metadata);
+  try {
+    const bookings = new Set();
+
+    for (let key of Object.keys(metadata)) {
+      if (key.startsWith("booking_")) {
+        const index = key.split("_")[1];
+        const bookingEntry = {
+          user_id: metadata[`booking_${index}_user_id`],
+          tour_id: metadata[`booking_${index}_tour_id`],
+          time_slot_id: metadata[`booking_${index}_time_slot_id`],
+          booking_date: metadata[`booking_${index}_booking_date`],
+          adults: Number(metadata[`booking_${index}_adults`]),
+          children: Number(metadata[`booking_${index}_children`]),
+          infants: Number(metadata[`booking_${index}_infants`]),
+          created_at: new Date(),
+          updated_at: new Date(),
+        };
+        bookings.add(JSON.stringify(bookingEntry));
+      }
+    }
+
+    const uniqueBookings = Array.from(bookings).map((booking) =>
+      JSON.parse(booking)
+    );
+
+    if (uniqueBookings.length > 0) {
+      await knex("bookings").insert(uniqueBookings);
+    }
+  } catch (error) {
+    console.error("Error adding bookings to database:", error);
+    throw error;
+  }
 }
 
 export default router;
